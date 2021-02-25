f5f448da16fe70da789c3f92052ad05fc3336978
Volkmar Rigo <volkmar.rigo@infominds.eu>
2021-02-25 18:51:48 +0100

    Input file

--- GoogleHashCode/Model/Input.cs
+++ GoogleHashCode/Model/Input.cs
@@ -3,9 +3,32 @@ using System.Linq;
 
 namespace GoogleHashCode.Model
 {
-	public record Input
+
+    public record Street
+    {
+        public int Begin;
+        public int End;
+        public string StreetName;
+        public int Length;
+    }
+
+    public record CarPath
+    {
+        public int NumberOfStreets;
+        public List<string> StreetNames;
+    }
+
+    public record Input
 	{
-		public static Input Parse(string[] values)
+        public int Duration;
+        public int NumberOfIntersections;
+        public int NumberOfStreets;
+        public int NumberOfCars;
+        public int BonusPoints;
+
+        public List<Street> Streets = new();
+
+        public static Input Parse(string[] values)
 		{
 			return new();
 		}
