1f10923b809a29839b716c77df254cf042c4eca4
Volkmar Rigo <volkmar.rigo@infominds.eu>
2021-02-25 19:21:53 +0100

    Output

--- GoogleHashCode/Model/Output.cs
+++ GoogleHashCode/Model/Output.cs
@@ -5,7 +5,7 @@ using GoogleHashCode.Base;
 
 namespace GoogleHashCode.Model
 {
-	public record StreetSchedule(string StreetName, int DurationGreem);
+	public record StreetSchedule(string StreetName, int DurationGreen);
 
 	public record Intersection(int ID, List<StreetSchedule> StreetsSchedule);
 
@@ -13,7 +13,20 @@ namespace GoogleHashCode.Model
 	{
 		public string[] GetOutputFormat()
 		{
-			return null;
+			var output = new List<string>();
+			output.Add(Intersections.Count.ToString());
+            foreach (var intersection in Intersections)
+            {
+				output.Add(intersection.ID.ToString());
+				output.Add(intersection.StreetsSchedule.Count.ToString());
+                foreach (var streetSchedule in intersection.StreetsSchedule)
+                {
+					output.Add(streetSchedule.StreetName);
+					output.Add(streetSchedule.DurationGreen.ToString());
+				}
+			}
+
+			return output.ToArray();
 		}
 
 		public int GetScore(Input input)
