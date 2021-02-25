340664ec3d047f113bbff9a11bf783d44f0fe87e
klyse <Klaus.Pruenster@gmail.com>
2021-02-25 19:21:06 +0100

    brute force

--- GoogleHashCode/Algorithms/Solver1.cs
+++ GoogleHashCode/Algorithms/Solver1.cs
@@ -10,7 +10,35 @@ namespace GoogleHashCode.Algorithms
 	{
 		public Output Solve(Input input)
 		{
-			return new();
+			var intersections = new List<Intersection>();
+
+
+			var intersectionsWithStreets = input.Streets
+				.GroupBy(c => new
+				{
+					Id = c.EndIntersectionID
+				})
+				.Select(c => new
+				{
+					c.Key.Id,
+					Streets = c.Select(r => r.StreetName)
+				});
+
+
+			foreach (var inputStreet in intersectionsWithStreets)
+			{
+				var schedule = new List<StreetSchedule>();
+				
+				foreach (var street in inputStreet.Streets)
+				{
+					schedule.Add(new StreetSchedule(street, 1));
+				}
+				
+				var intersection = new Intersection(inputStreet.Id, schedule);
+				intersections.Add(intersection);
+			}
+
+			return new Output(intersections);
 		}
 	}
 }
