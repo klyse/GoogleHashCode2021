08647f14148a751b964ec04894cb804b703a05e6
Volkmar Rigo <volkmar.rigo@infominds.eu>
2021-02-25 18:58:26 +0100

    Output

--- GoogleHashCode/Model/Output.cs
+++ GoogleHashCode/Model/Output.cs
@@ -5,9 +5,22 @@ using GoogleHashCode.Base;
 
 namespace GoogleHashCode.Model
 {
+	public record StreetSchedule
+    {
+		public string StreetName;
+		public int DurationGreem;
+	}
+
+	public record Intersection
+    {
+		public int ID;
+		public List<StreetSchedule> StreetsSchedule = new();
+    }
+
 
 	public record Output : IOutput
 	{
+		public List<Intersection> Intersections = new();
 
 		public string[] GetOutputFormat()
 		{
